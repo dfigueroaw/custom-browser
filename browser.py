a9e636b8ee0f75bb1fed017c818a665464613dbe
dfigueroaw <dfigueroaw@gmail.com>
2026-02-17 21:13:37 -0500

    feat: add redirect support

--- browser.py
+++ browser.py
@@ -1,6 +1,9 @@
 import socket
 import ssl
 
+class RedirectLoopError(Exception):
+    pass
+
 class URL:
     def __init__(self, url):
         self.scheme, url = url.split("://", 1)
@@ -37,52 +40,68 @@ class URL:
         if headers is None:
             headers = {}
 
-        s = socket.socket(
-            family=socket.AF_INET,
-            type=socket.SOCK_STREAM,
-            proto=socket.IPPROTO_TCP
-        )
+        redirect_count = 0
+        current_url = self
 
-        s.connect((self.host, self.port))
-        if self.scheme == "https":
-            ctx = ssl.create_default_context()
-            s = ctx.wrap_socket(s, server_hostname=self.host)
-
-        normalized_headers = {
-            "Host": self.host,
-            "Connection": "close",
-            "User-Agent": "CustomBrowser"
-        }
-
-        for header, value in headers.items():
-            normalized_headers[header.title()] = value
-
-        request = "GET {} HTTP/1.1\r\n".format(self.path)
-        for header, value in normalized_headers.items():
-            request += "{}: {}\r\n".format(header, value)
-        request += "\r\n"
-        s.send(request.encode("utf8"))
-
-        response = s.makefile("r", encoding="utf8", newline="\r\n")
-
-        statusline = response.readline()
-        version, status, explanation = statusline.split(" ", 2)
-
-        response_headers = {}
         while True:
-            line = response.readline()
-            if line == "\r\n":
-                break
-            header, value = line.split(":", 1)
-            response_headers[header.casefold()] = value.strip()
+            s = socket.socket(
+                family=socket.AF_INET,
+                type=socket.SOCK_STREAM,
+                proto=socket.IPPROTO_TCP
+            )
 
-        assert "transfer-encoding" not in response_headers
-        assert "content-encoding" not in response_headers
+            s.connect((current_url.host, current_url.port))
+            if current_url.scheme == "https":
+                ctx = ssl.create_default_context()
+                s = ctx.wrap_socket(s, server_hostname=current_url.host)
 
-        content = response.read()
-        s.close()
+            normalized_headers = {
+                "Host": current_url.host,
+                "Connection": "close",
+                "User-Agent": "CustomBrowser"
+            }
 
-        return content
+            for header, value in headers.items():
+                normalized_headers[header.title()] = value
+
+            request = "GET {} HTTP/1.1\r\n".format(current_url.path)
+            for header, value in normalized_headers.items():
+                request += "{}: {}\r\n".format(header, value)
+            request += "\r\n"
+            s.send(request.encode("utf8"))
+
+            response = s.makefile("r", encoding="utf8", newline="\r\n")
+
+            statusline = response.readline()
+            version, status, explanation = statusline.split(" ", 2)
+
+            response_headers = {}
+            while True:
+                line = response.readline()
+                if line == "\r\n":
+                    break
+                header, value = line.split(":", 1)
+                response_headers[header.casefold()] = value.strip()
+
+            assert "transfer-encoding" not in response_headers
+            assert "content-encoding" not in response_headers
+
+            content = response.read()
+            s.close()
+
+            if 300 <= int(status) < 400 and "location" in response_headers:
+                location = response_headers.get("location")
+                redirect_count += 1
+                if redirect_count > 10:
+                    raise RedirectLoopError("Infinite redirect loop")
+
+                if location.startswith("/"):
+                    location = "{}://{}:{}{}".format(current_url.scheme, current_url.host, current_url.port, location)
+
+                current_url = URL(location)
+                continue
+
+            return content
 
 def show(body):
     in_tag = False
